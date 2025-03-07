b3f70b0207d04a3cb114d5a537141ff226e1af42
Paolo Mangraviti <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2025-03-07 09:22:41 +0100

    Update testAlcol.ino

--- testAlcol.ino
+++ testAlcol.ino
@@ -1,6 +1,10 @@
 #define valoreNormale 90   
 #define valoreElevato 300  
-#define MQ3pin 0
+#define MQ3pin 0            // Pin A0, analogico 0
+                            //                            0
+                            //                     ---------------   
+                            //                     |   |    |    | 
+                            // sul sensore MQ-3   vcc gnd   nc  data
  
 float misuraCampione;  
  
@@ -12,13 +16,13 @@ void setup() {
  
 void loop() {
 	misuraCampione = analogRead(MQ3pin); 
- 
+	
 	Serial.print("Valore rilevato: "); Serial.print(misuraCampione);
 	
 	if (misuraCampione < valoreNormale) {	Serial.println("  |  Stato: normale"); } 
-  else 
-    if (campimisuraCampioneone >= valoreNormale && misuraCampione < valoreElevato) { Serial.println("  |  Stato: elevato"); } 
-    else { Serial.println("  |  Stato: eccessivo");	}
- 
+	else 
+		if (misuraCampione >= valoreNormale && misuraCampione < valoreElevato) { Serial.println("  |  Stato: elevato"); } 
+		else { Serial.println("  |  Stato: eccessivo");	}
+	
 	delay(2000); 
 }
